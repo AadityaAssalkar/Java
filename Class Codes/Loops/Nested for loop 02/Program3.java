bd4b4c6d9c63b4688bc1b5b8c0e310c09956054f
AadiBhai <aadityaassalkar@gmail.com>
2023-08-29 20:31:20 +0530

    class code nested for2 executed

---
+++ Class Codes/Loops/Nested for loop 02/Program3.java
@@ -0,0 +1,18 @@
+// A   1   B   2   
+// A   1   B   2   
+// A   1   B   2   
+
+class Demo {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 3; i++) {
+			char ch = 'A'; int num = 1;
+			for(int j = 1; j <= 4; j++) {
+				if(j%2 == 0)
+					System.out.print(num++ + "   ");
+				else
+					System.out.print(ch++ + "   ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Class Codes/Loops/Nested for loop 02/Program4.java
@@ -0,0 +1,20 @@
+// A   1   B   2   
+// C   3   D   4   
+// E   5   F   6
+
+class Demo {
+	public static void main(String[] args) {
+		char ch = 'A';
+		int num = 1;
+
+		for(int i = 1; i <= 3; i++) {
+			for(int j = 1; j <= 4; j++) {
+				if(j%2 == 0)
+					System.out.print(num++ + "   ");
+				else
+					System.out.print(ch++ + "   ");
+			}
+			System.out.println();
+		}
+	}
+}
