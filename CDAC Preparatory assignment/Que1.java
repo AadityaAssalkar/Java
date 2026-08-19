d68139d18ef4a085bcbab5d8488d5b7d23f7ecb5
Aaditya Assalkar <94801359+AadityaAssalkar@users.noreply.github.com>
2026-08-19 21:50:38 +0530

    Refactor MaxNum to accept command-line arguments

--- CDAC Preparatory assignment/Que1.java
+++ CDAC Preparatory assignment/Que1.java
@@ -2,22 +2,18 @@
 // Accept n numbers through command-line arguments and find and display the maximum number.
 // Note: Assume that at least one number is provided.
 
-import java.util.*;
-
 class MaxNum{
   public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.println("How many numbers do you want to compare?");
-    int n = sc.nextInt();
-    int max = Integer.MIN_VALUE;
+    int max = Integer.parseInt(args[0]);
 
-    for(int i = 0; i < n; i++){
-      int num = sc.nextInt();
-      if(num > max){
-        max = num;
-      }
-    }
+        for (int i = 1; i < args.length; i++) {
+            int num = Integer.parseInt(args[i]);
 
-    System.out.println("Maximum Number: " + max);
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        System.out.println("Maximum number: " + max);
   }
 }
